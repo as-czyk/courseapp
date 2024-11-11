ff04a200cd7c758b0fe6eac97b8cdd13601dd1df
asczyk <aron.scheffczyk@gmail.com>
2024-11-11 15:25:53 +0100

    add back button on home screen

--- course-app/app/(auth)/home.tsx
+++ course-app/app/(auth)/home.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import ButtonUI from "@/components/Button";
+import { Routes } from "@/constants/Routes";
+import { useRouter } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
+      <ButtonUI title={"Back"} onPress={() => router.push(Routes.INDEX)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+});
